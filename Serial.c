de55a693b604d9d14529f3058552fa2562d2a44f
alexander <alexanderdeurloo@gmail.com>
2021-03-01 18:18:20 +0000

    Cpu connect retries. No COM reset yet

--- Serial.c
+++ Serial.c
@@ -88,7 +88,7 @@ int SerialTx(char *Command, size_t CommandSize)
         {
         
            write (fd, Command, CommandSize); 
-           printf("We've sent: %s with size %i\n",Command, CommandSize); 
+           printf("We've sent: %s   with size %i\n",Command, CommandSize); 
            return 0;
         }
         printf("fd wasn't okay\n");
@@ -120,38 +120,21 @@ int SerialClose(void)
 
 
 
-int SerialTxRx(char *portname, char *DataToSend, char *Result, size_t ResultLength)
+int SerialTxRx(char *portname, char *DataToSend, char *Result, size_t ResultLength, int Blocking)
 {
         char Command[100];
         int receivedlength = 0;
         if(portname==NULL) return -1;
         if(DataToSend==NULL) return -2;
         if(Result==NULL) return -3;
-
-        if (SerialOpen(portname,B4800,0,1))
+        if ( (Blocking < 0) || (Blocking > 1) ) Blocking = 1;
+        
+        if (SerialOpen(portname,B4800,0,Blocking))
         {
            printf("Error opening serial\n");
            return -4;
         }
-        /*
-        if(argc)
-        {
-                if(argv==NULL)
-                {
-                        printf("Whoops. no valid string\n");
-                        return -4;
-                }
-                //printf("Argc found\n");
-                sprintf(Command,"%s\n",argv);
-        }
-        else
-        {
-                //printf("no argc found\n");
-                sprintf(Command,"waterlevel?\n");
-        }
-        */
         sprintf(Command,"%s",DataToSend);
-        //printf("Send: >%s<\n",Command);
         SerialTx(Command,strlen(Command));
         usleep(100000);
         Command[0] = 0;
@@ -160,7 +143,6 @@ int SerialTxRx(char *portname, char *DataToSend, char *Result, size_t ResultLeng
         {
            if(receivedlength > ResultLength) receivedlength = ResultLength;
            snprintf(Result,receivedlength,Command);
-           //Result[receivedlength] = 0;
         }
         SerialClose();
         return receivedlength;
@@ -186,112 +168,4 @@ int SerialTxStirrer(char *portname, unsigned char *DataToSend, size_t Bytes)
         return 0;
 
 }
-
-void printdata(char* data, size_t length)
-{
-    int i;
-    for(i=0;i<length;i++)
-        printf("%i = 0x%02x\n",i,data[i]);
-}
-
-int SerialTxSlow(char *Command, size_t CommandSize)
-{
-        if(Command == NULL) return -1;
-        if (CommandSize == 0) return -2;
-        int i = 0;
-        int byte;
-        if(fd)
-        {
-            for (i=0;i<CommandSize;i++)
-            {
-                byte = Command[i];
-               write (fd,(void*) byte, 1);
-               printf("%i = 0x%02x\n",i,byte); 
-               //usleep(5000);
-            }
-           //printf("We've sent: %s with size %i\n",Command, CommandSize); 
-           return 0;
-        }
-        printf("fd wasn't okay\n");
-        return -1;
-}
-
-
-int main(void)
-{
-    char *portname = "/dev/ttyUSB2";
-    char StirrerData[10];
-    int receivedlength = 0;
-    unsigned char checksum;
-    unsigned int RpmSetting;
-    if (SerialOpen(portname,B9600,0,0)) // no blocking
-    {
-       printf("Error opening serial\n");
-       return -3;
-    }
-    int i;
-    // get status
-/*    StirrerData[0] = 0xFE;
-    StirrerData[1] = 0xA1; //get status
-    StirrerData[2] = 0x00;
-    StirrerData[3] = 0x00;
-    StirrerData[4] = 0x00;
-    StirrerData[5] = 0xA1;
-    StirrerData[6] = 0x00;
-    SerialTxSlow(StirrerData,6);
-    sleep(1);
-    */
-    // 0 rpm
-    StirrerData[0] = 0xFE;
-    StirrerData[1] = 0xB1;
-    StirrerData[2] = 0x00;
-    StirrerData[3] = 0x00;
-    StirrerData[4] = 0x00;
-    StirrerData[5] = 0xB1;
-    StirrerData[6] = 0x00;
-    SerialTxSlow(StirrerData,6);
-    sleep(1);
-    
-    RpmSetting = 100;
-    StirrerData[0] = 0xFE;
-    StirrerData[1] = 0xB1;
-    StirrerData[2] = (RpmSetting >> 8) & 0xFF;
-    StirrerData[3] = RpmSetting & 0xFF;
-    StirrerData[4] = 0x00;
-    checksum = (StirrerData[1] + StirrerData[2] + StirrerData[3] + StirrerData[4]) & 0xFF;
-    StirrerData[5] = checksum;
-    StirrerData[6] = 0x00;
-    SerialTxSlow(StirrerData,6);
-    
-// mirror all bytes:
-/*
-    StirrerData[0] = 0x7F;
-    StirrerData[1] = 0x83;
-    StirrerData[2] = 0x00;
-    StirrerData[3] = 0x00;
-    StirrerData[4] = 0x00;
-    StirrerData[5] = 0x83;
-    StirrerData[6] = 0x00;
-    SerialTxSlow(StirrerData,6);
-    sleep(1);
-    
-    RpmSetting = 100;
-    StirrerData[0] = 0x7F;
-    StirrerData[1] = 0x83;
-    StirrerData[2] = 0x00;
-    StirrerData[3] = 0x26;
-    StirrerData[4] = 0x00;
-    checksum = (StirrerData[1] + StirrerData[2] + StirrerData[3] + StirrerData[4]) & 0xFF;
-    StirrerData[5] = 0x9B;
-    StirrerData[6] = 0x00;
-    SerialTxSlow(StirrerData,7);
-    */
-    SerialClose();
-    return 0;
-}
-
-
-
-
-
 
